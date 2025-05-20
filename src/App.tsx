b81e00e0949fee23d92b515d32e010b2c40e8bf3
YehuditGrinwald <100yehudit@gmail.com>
2025-05-20 22:48:48 +0300

    statistics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import NavBar from './components/NavBar';
+import NavBar from './components/NavBar';
 import StatisticsPage from './pages/statistics/StatisticsPage';
 import UsersPage from './pages/users/UsersPage';
 import { UsersProvider } from './context/usersContext';
@@ -8,7 +8,7 @@ import { UsersProvider } from './context/usersContext';
 function App() {
   return (
     <BrowserRouter>
-      {/* <NavBar /> */}
+      <NavBar />
       <UsersProvider>
         <Routes>
           <Route path="/" element={<StatisticsPage />} />
--- src/pages/statistics/StatisticsPage.tsx
+++ src/pages/statistics/StatisticsPage.tsx
@@ -1,9 +1,38 @@
 import { useContext } from 'react';
 import styles from './statistics.module.css';
+import { useUsersContext } from '../../context/usersContext';
 
 function StatisticsPage() {
-  // You can now use users for statistics rendering
-  return <div className={styles.pageRoot}>StatisticsPage</div>;
+  const { users, isLoading } = useUsersContext();
+
+  const countryStats = users.reduce((acc: { [key: string]: number }, user) => {
+    acc[user.country] = (acc[user.country] || 0) + 1;
+    return acc;
+  }, {});
+
+  if (isLoading) {
+    return <div className={styles.pageRoot}>Loading...</div>;
+  }
+
+  return (
+    <div className={styles.pageRoot}>
+      <div className={styles.statsContainer}>
+        <h2>Users by Country</h2>
+        <ul className={styles.statsList}>
+          {Object.entries(countryStats)
+            .sort()
+            .map(([country, count]) => (
+              <li key={country} className={styles.statsListItem}>
+                <span>{country}</span>
+                <span>
+                  {count} user{count !== 1 ? 's' : ''}
+                </span>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default StatisticsPage;
